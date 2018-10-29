38e372c23ba82aee91c98e887169fb1476128a2d
장혁_집 <reaper83@naver.com>
2018-10-30 02:12:08 +0900

    @ 인버터 메뉴 구조 설계

--- routes/users/inverter.js
+++ routes/users/inverter.js
@@ -31,30 +31,29 @@ router.get(
     // 모든 인버터 조회하고자 할 경우 Id를 지정하지 않음
     const pwProfileWhereInfo = _.eq(siteId, 'all') ? null : { main_seq: siteId };
 
+    // Power 현황 테이블에서 선택한 Site에 속해있는 인버터 목록을 가져옴
     /** @type {V_PW_PROFILE[]} */
     const viewPowerProfileRows = await biModule.getTable('v_pw_profile', pwProfileWhereInfo, false);
     const inverterSeqList = _.map(viewPowerProfileRows, 'inverter_seq');
-    BU.CLI(inverterSeqList);
+    // BU.CLI(inverterSeqList);
 
     /** @type {V_INVERTER_STATUS[]} */
-    const viewInverterStatusList = await biModule.getTable('v_pw_inverter_status', {
+    const viewInverterStatusRows = await biModule.getTable('v_pw_inverter_status', {
       inverter_seq: inverterSeqList,
     });
 
-    BU.CLI(viewInverterStatusList);
-
     // 데이터 검증
-    const validInverterStatus = webUtil.checkDataValidation(
-      viewInverterStatusList,
+    const validInverterStatusList = webUtil.checkDataValidation(
+      viewInverterStatusRows,
       new Date(),
       'writedate',
     );
 
-    BU.CLI(validInverterStatus);
+    // BU.CLI(validInverterStatus);
 
     /** 인버터 메뉴에서 사용 할 데이터 선언 및 부분 정의 */
-    const refinedInverterStatusList = webUtil.refineSelectedInverterStatus(validInverterStatus);
-    BU.CLI(refinedInverterStatusList);
+    const refinedInverterStatusList = webUtil.refineSelectedInverterStatus(validInverterStatusList);
+    // BU.CLI(refinedInverterStatusList);
 
     const searchRange = biModule.getSearchRange('min10');
     // searchRange.searchInterval = 'day';
