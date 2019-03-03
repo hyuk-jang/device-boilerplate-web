8067f61f52099d6ce29d247f68adc3488354176e
장혁_집 <reaper83@naver.com>
2019-03-03 23:40:45 +0900

    @ 리뉴얼 수정

--- public/js/contents/main/hichartMaker.js
+++ public/js/contents/main/hichartMaker.js
@@ -10,7 +10,7 @@ const gaugeOptions = {
     startAngle: -90,
     endAngle: 90,
     background: {
-      backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#fff',
+      backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
       innerRadius: '60%',
       outerRadius: '100%',
       shape: 'arc',
@@ -47,6 +47,62 @@ const gaugeOptions = {
         useHTML: true,
       },
     },
+  },
+};
+
+const defaultGaugeOptions = {
+  chart: {
+    type: 'solidgauge',
+  },
+
+  title: null,
+
+  pane: {
+    center: ['50%', '85%'],
+    size: '140%',
+    startAngle: -90,
+    endAngle: 90,
+    background: {
+      backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+      innerRadius: '60%',
+      outerRadius: '100%',
+      shape: 'arc',
+    },
+  },
+
+  tooltip: {
+    enabled: false,
+  },
+
+  // the value axis
+  yAxis: {
+    stops: [
+      [0.1, '#55BF3B'], // green
+      [0.5, '#DDDF0D'], // yellow
+      [0.9, '#DF5353'], // red
+    ],
+    lineWidth: 0,
+    minorTickInterval: null,
+    tickAmount: 2,
+    title: {
+      y: -70,
+    },
+    labels: {
+      y: 16,
+    },
+  },
+
+  plotOptions: {
+    solidgauge: {
+      dataLabels: {
+        y: 5,
+        borderWidth: 0,
+        useHTML: true,
+      },
+    },
+  },
+  credits: {
+    enabled: false,
   },
 };
 
