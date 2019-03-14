5ef91f56cfdaac8269d0ffe6f3441c802c0a3dfa
장혁 <hyuk.jang@smsoft.co.kr>
2019-03-14 14:59:04 +0900

    @ babel 처리

--- public/js/contents/inverter/setInverter.js
+++ public/js/contents/inverter/setInverter.js
@@ -5,31 +5,14 @@
  * @param {string} siteId
  */
 function setInverterList(domElement, inverterStatusList) {
-  const inverterStatusTemplate = _.template(`
-    <tr class="sel">
-    <td scope="row"><%= siteName %></td>
-    <td > <%= inclinedSolar %> </td>
-    <td> <%= pv_a %> </td>
-    <td> <%= pv_v %> </td>
-    <td> <%= pv_kw %> </td>
-    <td> <%= grid_r_a %> </td>
-    <td> <%= grid_rs_v %> </td>
-    <td> <%= line_f %> </td>
-    <td> <%= power_kw %> </td>
-    <td> <%= power_f %> </td>
-    <td> <%= daily_power_kwh %> </td>
-    <td> <%= power_c_kwh %> </td>
-    <td class="center_ball">
-      <img src="/image/<%= operImgName %>" />
-    </td>
-  </tr>
-    `);
-  const optionList = inverterStatusList.dataList.map(inverterStatusInfo => {
+  const inverterStatusTemplate = _.template(
+    '\n    <tr class="sel">\n    <td scope="row"><%= siteName %></td>\n    <td > <%= inclinedSolar %> </td>\n    <td> <%= pv_a %> </td>\n    <td> <%= pv_v %> </td>\n    <td> <%= pv_kw %> </td>\n    <td> <%= grid_r_a %> </td>\n    <td> <%= grid_rs_v %> </td>\n    <td> <%= line_f %> </td>\n    <td> <%= power_kw %> </td>\n    <td> <%= power_f %> </td>\n    <td> <%= daily_power_kwh %> </td>\n    <td> <%= power_c_kwh %> </td>\n    <td class="center_ball">\n      <img src="/image/<%= operImgName %>" />\n    </td>\n  </tr>\n    ',
+  );
+
+  const optionList = inverterStatusList.dataList.map(function(inverterStatusInfo) {
     const operImgName = inverterStatusInfo.isOperation ? 'green.png' : 'red.png';
     inverterStatusInfo.operImgName = operImgName;
-
     return inverterStatusTemplate(inverterStatusInfo);
   });
-  $(domElement).html(optionList);
-  // domElement.innerHTML = optionList;
+  $(domElement).html(optionList); // domElement.innerHTML = optionList;
 }
--- public/js/contents/report/search.js
+++ public/js/contents/report/search.js
@@ -1,26 +1,25 @@
-$('#searchType').on('change', function() {
+"use strict";
+
+$('#searchType').on('change', function () {
   applySearchType($(this).val());
-});
+}); // 생육 환경, 인버터 클릭 시 페이지 이동
 
-// 생육 환경, 인버터 클릭 시 페이지 이동
-$('#subCategoryDom')
-  .children()
-  .each((index, dom) => {
-    $(dom).on('click', function() {
-      location.href = `${window.location.origin}/${naviId}/${siteId}/${$(this).val()}`;
-    });
+$('#subCategoryDom').children().each(function (index, dom) {
+  $(dom).on('click', function () {
+    location.href = "".concat(window.location.origin, "/").concat(naviId, "/").concat(siteId, "/").concat($(this).val());
   });
-
-$('#downloadExcel').on('click', event => {
-  let subCategoryId = document.querySelector('#subSelectBoxDom option:checked').value;
+});
+$('#downloadExcel').on('click', function (event) {
+  var subCategoryId = document.querySelector('#subSelectBoxDom option:checked').value;
   subCategoryId = subCategoryId.trim();
-  const searchInterval = document.querySelector('#searchInterval option:checked').value;
-  const searchType = document.querySelector('#searchType option:checked').value;
-  const strStartDateInputValue = document.getElementById('strStartDateInputValue').value;
-  let strEndDateInputValue = '';
+  var searchInterval = document.querySelector('#searchInterval option:checked').value;
+  var searchType = document.querySelector('#searchType option:checked').value;
+  var strStartDateInputValue = document.getElementById('strStartDateInputValue').value;
+  var strEndDateInputValue = '';
 
   if (searchType === 'range') {
     strEndDateInputValue = document.getElementById('strEndDateInputValue').value;
+
     if (strStartDateInputValue > strEndDateInputValue) {
       return alert('종료일이 시작일보다 빠를 수 없습니다.');
     }
@@ -30,35 +29,31 @@ $('#downloadExcel').on('click', event => {
     return alert('엑셀 Download는 1분, 10분, 1시간 단위 생성만 지원합니다.');
   }
 
-  const dataParam = {
-    searchInterval,
-    searchType,
-    strStartDateInputValue,
-    strEndDateInputValue,
+  var dataParam = {
+    searchInterval: searchInterval,
+    searchType: searchType,
+    strStartDateInputValue: strStartDateInputValue,
+    strEndDateInputValue: strEndDateInputValue
   };
-
   $('#loader').removeClass('hidden');
   $('#loader-ground').removeClass('hidden');
-
   $.ajax({
     type: 'GET',
-    url: `${window.location.origin}/${naviId}/${siteId}/${subCategory}/${subCategoryId}/` + 'excel',
-    data: dataParam,
-  })
-    .done(excelData => {
-      $('#loader').addClass('hidden');
-      $('#loader-ground').addClass('hidden');
-      // console.log(excelData);
-      XLSX.writeFile(excelData.workBook, `${excelData.fileName}.xlsx`);
-    })
-    .fail((req, sts, err) => {
-      alert(err);
-    });
+    url: "".concat(window.location.origin, "/").concat(naviId, "/").concat(siteId, "/").concat(subCategory, "/").concat(subCategoryId, "/") + 'excel',
+    data: dataParam
+  }).done(function (excelData) {
+    $('#loader').addClass('hidden');
+    $('#loader-ground').addClass('hidden'); // console.log(excelData);
+
+    XLSX.writeFile(excelData.workBook, "".concat(excelData.fileName, ".xlsx"));
+  }).fail(function (req, sts, err) {
+    alert(err);
+  });
 });
 
 function checkSelectBoxOption(selectBoxId, selectValue) {
-  const $selectBoxOptions = $(`#${selectBoxId}`).children();
-  $selectBoxOptions.each((index, dom) => {
+  var $selectBoxOptions = $("#".concat(selectBoxId)).children();
+  $selectBoxOptions.each(function (index, dom) {
     // dom.getAttribute('value')
     if (dom.getAttribute('value') === selectValue) {
       dom.selected = true;
@@ -67,26 +62,21 @@ function checkSelectBoxOption(selectBoxId, selectValue) {
     }
   });
 }
-
 /**
  * 검색 기간 Radio 클릭 시 날짜 영역 설정
  * @param {Dom} input[name='searchType']
  * @return {void}
  */
+
+
 function applySearchType(value) {
-  const checkedSearchType = value;
-  const startDateDom = document.querySelector('#strStartDateInputValue');
-  const endDateDom = document.querySelector('#strEndDateInputValue');
-
-  const startDate = new Date(searchRange.strStartDateInputValue);
-  const endDate =
-    searchRange.strEndDateInputValue === '' ||
-    new Date(searchRange.strEndDateInputValue) === 'Invalid Date'
-      ? startDate
-      : new Date(searchRange.strEndDateInputValue);
-
-  let viewMode = 0;
-  let sliceEndIndex = 10;
+  var checkedSearchType = value;
+  var startDateDom = document.querySelector('#strStartDateInputValue');
+  var endDateDom = document.querySelector('#strEndDateInputValue');
+  var startDate = new Date(searchRange.strStartDateInputValue);
+  var endDate = searchRange.strEndDateInputValue === '' || new Date(searchRange.strEndDateInputValue) === 'Invalid Date' ? startDate : new Date(searchRange.strEndDateInputValue);
+  var viewMode = 0;
+  var sliceEndIndex = 10;
 
   if (checkedSearchType === 'range') {
     $('#strEndDateInputValue').show();
@@ -112,56 +102,53 @@ function applySearchType(value) {
     viewMode = 0;
     sliceEndIndex = 10;
   }
+
   startDateDom.value = startDate.toISOString().substring(0, sliceEndIndex);
   makeDatePicker(startDateDom, viewMode);
-}
+} // 검색 클릭 시
 
-// 검색 클릭 시
+
 function searchReport() {
-  let subCategoryId = document.querySelector('#subSelectBoxDom option:checked').value;
+  var subCategoryId = document.querySelector('#subSelectBoxDom option:checked').value;
   subCategoryId = subCategoryId.trim();
-  const searchInterval = document.querySelector('#searchInterval option:checked').value;
-  const searchType = document.querySelector('#searchType option:checked').value;
-  const strStartDateInputValue = document.getElementById('strStartDateInputValue').value;
-  let strEndDateInputValue = '';
+  var searchInterval = document.querySelector('#searchInterval option:checked').value;
+  var searchType = document.querySelector('#searchType option:checked').value;
+  var strStartDateInputValue = document.getElementById('strStartDateInputValue').value;
+  var strEndDateInputValue = '';
 
   if (searchType === 'range') {
     strEndDateInputValue = document.getElementById('strEndDateInputValue').value;
+
     if (strStartDateInputValue > strEndDateInputValue) {
       return alert('종료일이 시작일보다 빠를 수 없습니다.');
     }
   }
 
-  const queryString = `searchType=${searchType}&searchInterval=${searchInterval}&strStartDateInputValue=${strStartDateInputValue}&strEndDateInputValue=${strEndDateInputValue}`;
-
+  var queryString = "searchType=".concat(searchType, "&searchInterval=").concat(searchInterval, "&strStartDateInputValue=").concat(strStartDateInputValue, "&strEndDateInputValue=").concat(strEndDateInputValue);
   $('#loader').removeClass('hidden');
-  $('#loader-ground').removeClass('hidden');
+  $('#loader-ground').removeClass('hidden'); // 사이트 변경 시
 
-  // 사이트 변경 시
-  location.href = `${
-    window.location.origin
-  }/${naviId}/${siteId}/${subCategory}/${subCategoryId}?${queryString}`;
-}
+  location.href = "".concat(window.location.origin, "/").concat(naviId, "/").concat(siteId, "/").concat(subCategory, "/").concat(subCategoryId, "?").concat(queryString);
+} // 검색 클릭 시
 
-// 검색 클릭 시
+
 function searchTrend() {
-  const searchInterval = document.querySelector('#searchInterval option:checked').value;
-  const searchType = document.querySelector('#searchType option:checked').value;
-  const strStartDateInputValue = document.getElementById('strStartDateInputValue').value;
-  let strEndDateInputValue = '';
+  var searchInterval = document.querySelector('#searchInterval option:checked').value;
+  var searchType = document.querySelector('#searchType option:checked').value;
+  var strStartDateInputValue = document.getElementById('strStartDateInputValue').value;
+  var strEndDateInputValue = '';
 
   if (searchType === 'range') {
     strEndDateInputValue = document.getElementById('strEndDateInputValue').value;
+
     if (strStartDateInputValue > strEndDateInputValue) {
       return alert('종료일이 시작일보다 빠를 수 없습니다.');
     }
   }
 
-  const queryString = `searchType=${searchType}&searchInterval=${searchInterval}&strStartDateInputValue=${strStartDateInputValue}&strEndDateInputValue=${strEndDateInputValue}`;
-
+  var queryString = "searchType=".concat(searchType, "&searchInterval=").concat(searchInterval, "&strStartDateInputValue=").concat(strStartDateInputValue, "&strEndDateInputValue=").concat(strEndDateInputValue);
   $('#loader').removeClass('hidden');
-  $('#loader-ground').removeClass('hidden');
+  $('#loader-ground').removeClass('hidden'); // 사이트 변경 시
 
-  // 사이트 변경 시
-  location.href = `${window.location.origin}/${naviId}/${siteId}?${queryString}`;
+  location.href = "".concat(window.location.origin, "/").concat(naviId, "/").concat(siteId, "?").concat(queryString);
 }
