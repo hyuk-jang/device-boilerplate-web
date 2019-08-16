47aee31abb2b86acf81f551063358d851297f7e7
장혁 <hyuk.jang@smsoft.co.kr>
2019-08-16 12:04:32 +0900

    @ 수정 중

--- models/domMaker/inverterDom.js
+++ models/domMaker/inverterDom.js
@@ -5,6 +5,40 @@ const { BU } = require('base-util-jh');
 module.exports = {
   /**
    * 인버터 현재 상태
+   * @param {V_PW_INVERTER_STATUS[]} inverterStatusList
+   * @param {string} siteId
+   */
+  makeCurrInverterStatus(inverterStatusList) {
+    const inverterStatusTemplate = _.template(`
+    <tr>
+    <td class="table_title" title="<%= siteName %>"><%= siteName %></td>
+    <td> <%= pv_a %> </td>
+    <td> <%= pv_v %> </td>
+    <td> <%= pv_kw %> </td>
+    <td> <%= grid_r_a %> </td>
+    <td> <%= grid_rs_v %> </td>
+    <td> <%= line_f %> </td>
+    <td> <%= power_kw %> </td>
+    <td> <%= power_f %> </td>
+    <td> <%= daily_power_kwh %> </td>
+    <td> <%= power_cp_kwh %> </td>
+    <td class="text-center">
+      <img src="/image/<%= operImgName %>" />
+    </td>
+  </tr>
+    `);
+    const madeDom = inverterStatusList.map(inverterStatusInfo => {
+      BU.toLocaleString(inverterStatusInfo);
+      const operImgName = inverterStatusInfo.isOperation ? 'green.png' : 'red.png';
+      inverterStatusInfo.operImgName = operImgName;
+
+      return inverterStatusTemplate(inverterStatusInfo);
+    });
+    return madeDom;
+  },
+
+  /**
+   * 인버터 현재 상태
    * @param {{dataList: V_PW_INVERTER_STATUS[], totalInfo: {pv_kw: number, grid_kw: number, d_kwh: number, m_kwh: number}}} inverterStatusList
    * @param {string} siteId
    */
--- routes/upsas/main.js
+++ routes/upsas/main.js
@@ -21,53 +21,50 @@ router.get(
   ['/', '/main', '/main/:siteId'],
   asyncHandler(async (req, res) => {
     commonUtil.applyHasNumbericReqToNumber(req);
-    /** @type {BiModule} */
-    const biModule = global.app.get('biModule');
-    /** @type {PowerModel} */
-    const powerModel = global.app.get('powerModel');
     /** @type {RefineModel} */
     const refineModel = global.app.get('refineModel');
 
     // Site Sequence.지점 Id를 불러옴
     const { siteId } = req.locals.mainInfo;
 
-    // 모든 인버터 조회하고자 할 경우 Id를 지정하지 않음
-    const mainWhere = _.isNumber(siteId) ? { main_seq: siteId } : null;
+    /** @type {V_PW_PROFILE[]} powerProfileRows */
+    const powerProfileRows = req.locals.viewPowerProfileRows;
 
     // ********** Power 관련
-    /** @type {V_PW_PROFILE[]} */
-    const powerProfileRows = _.filter(req.locals.viewPowerProfileRows, mainWhere);
-
     /** @type {searchRange} */
     const searchRangeInfo = _.get(req, 'locals.searchRange');
 
     // BU.CLI(searchRangeInfo);
 
     // 발전 현황을 나타내는 기본적인 정보
-    const { powerGenerationInfo, validInverterDataList } = await refineModel.getGeneralPowerInfo(
+    const { powerGenerationInfo, validInverterDataList } = await refineModel.refineGeneralPowerInfo(
       siteId,
     );
 
     const inverterSeqList = _.map(powerProfileRows, 'inverter_seq');
 
     // 인버터 평균 출력 현황 차트로 긁어옴
-    const inverterLineChart = await refineModel.getInverterChart(searchRangeInfo, inverterSeqList, {
-      domId: 'daily_kw_graph',
-      // title: '인버터 발전 현황',
-      yAxisList: [
-        {
-          dataUnit: 'kW',
-          yTitle: '전력(kW)',
+    const inverterLineChart = await refineModel.refineInverterChart(
+      searchRangeInfo,
+      inverterSeqList,
+      {
+        domId: 'daily_kw_graph',
+        // title: '인버터 발전 현황',
+        yAxisList: [
+          {
+            dataUnit: 'kW',
+            yTitle: '전력(kW)',
+          },
+        ],
+        chartOption: {
+          selectKey: 'avg_grid_kw',
+          dateKey: 'group_date',
+          // groupKey: 'inverter_seq',
+          colorKey: 'chart_color',
+          sortKey: 'chart_sort_rank',
         },
-      ],
-      chartOption: {
-        selectKey: 'avg_grid_kw',
-        dateKey: 'group_date',
-        // groupKey: 'inverter_seq',
-        colorKey: 'chart_color',
-        sortKey: 'chart_sort_rank',
       },
-    });
+    );
 
     // BU.CLIN(inverterLineChart, 3);
 
