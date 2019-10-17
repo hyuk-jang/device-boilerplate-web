b699873b3521a1d3953c991e3c79c3f8c162682b
yangjongbin <jb.yang@smsoft.co.kr>
2019-10-17 18:03:42 +0900

    @ 회원가입 화면 추가 변경중

--- routes/auth.js
+++ routes/auth.js
@@ -69,7 +69,7 @@ router.post(
     // BU.CLIS('tempJoin', req.body, req.query, req.params);
     /** @type {BiAuth} */
     const biAuth = global.app.get('biAuth');
-
+    // BU.CLI(req.body);
     const { password = '', userid = '', name = '' } = _.pick(req.body, [
       'userid',
       'password',
@@ -90,8 +90,8 @@ router.post(
 
     // 동일한 회원이 존재하는지 체크
     const memberInfo = await biAuth.getTable('MEMBER', whereInfo);
-    BU.CLI(memberInfo);
-    if (_.isEmpty(memberInfo)) {
+    // BU.CLI(memberInfo);
+    if (!_.isEmpty(memberInfo)) {
       return res.status(500).send(DU.locationAlertGo('다른 ID를 입력해주세요.', '/login'));
     }
 
@@ -105,12 +105,45 @@ router.post(
     }
 
     /** @type {MEMBER} */
-    // FIXME: main_seq 수정
-    const newMemberInfo = { user_id: userid, name, main_seq: 1 };
+    // FIXME: main_seq 수정, grade 고정
+    const newMemberInfo = { user_id: userid, name, main_seq: 1, grade: 'guest', is_deleted: 0 };
 
     await biAuth.setMember(password, newMemberInfo);
 
-    return res.redirect(`/${SITE_HEADER}login`);
+    return res.send(DU.locationAlertGo('가입이 완료되었습니다.', `/${SITE_HEADER}login`));
+    // return res.redirect(`/${SITE_HEADER}login`);
+  }),
+);
+
+// TODO: 아이디 체크
+router.post(
+  '/temp-join/id-check',
+  asyncHandler(async (req, res) => {
+    /** @type {BiAuth} */
+    const biAuth = global.app.get('biAuth');
+    const { userid } = req.body;
+
+    /** @type {MEMBER} */
+    const whereInfo = {
+      user_id: userid,
+      is_deleted: 0,
+    };
+
+    if (_.isEmpty(userid)) {
+      return res.send('<p class="color_red">필수 정보입니다.</p>');
+    }
+
+    // 동일한 회원이 존재하는지 체크
+    const memberInfo = await biAuth.getTable('MEMBER', whereInfo);
+    if (!_.isEmpty(memberInfo))
+      return res.send('<p class="color_red">이미 사용중인 아이디입니다.</p>');
+
+    // id 정규식
+    const idReg = /^[A-Za-z0-9]{4,12}$/;
+    if (!idReg.test(userid))
+      return res.send('<p class="color_red">4~12자의 영문, 숫자로만 사용 가능합니다.</p>');
+
+    return res.send('<p class="color_green">사용 가능한 아이디 입니다.</p>');
   }),
 );
 
